555bdc1c27e2e56ac2102c823755cc17048f6e4d
eabanonu <eabanonu@yahoo.com>
2020-01-20 15:27:18 +0100

    CommandSeekBuilder

---
+++ SharpPulsar/Command/Builder/CommandActiveConsumerChangeBuilder.cs
@@ -0,0 +1,34 @@
+﻿using SharpPulsar.Common.PulsarApi;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpPulsar.Command.Builder
+{
+    public class CommandActiveConsumerChangeBuilder
+    {
+        public CommandActiveConsumerChange _change;
+        public CommandActiveConsumerChangeBuilder()
+        {
+            _change = new CommandActiveConsumerChange();
+        }
+        private CommandActiveConsumerChangeBuilder(CommandActiveConsumerChange change)
+        {
+            _change = change;
+        }
+        public CommandActiveConsumerChangeBuilder SetConsumerId(long consumerId)
+        {
+            _change.ConsumerId = (ulong)consumerId;
+            return new CommandActiveConsumerChangeBuilder(_change);
+        }
+        public CommandActiveConsumerChangeBuilder SetIsActive(bool isActive)
+        {
+            _change.IsActive = isActive;
+            return new CommandActiveConsumerChangeBuilder(_change);
+        }
+        public CommandActiveConsumerChange Build()
+        {
+            return _change;
+        }
+    }
+}
---
+++ SharpPulsar/Command/Builder/CommandSeekBuilder.cs
@@ -0,0 +1,44 @@
+﻿using SharpPulsar.Common.PulsarApi;
+
+namespace SharpPulsar.Command.Builder
+{
+    public class CommandSeekBuilder
+    {
+        private readonly CommandSeek _seek;
+        public CommandSeekBuilder()
+        {
+            _seek = new CommandSeek();
+        }
+        private CommandSeekBuilder(CommandSeek seek)
+        {
+            _seek = seek;
+        }
+        public CommandSeekBuilder SetConsumerId(long consumerid)
+        {
+            _seek.ConsumerId = (ulong)consumerid;
+            return new CommandSeekBuilder(_seek);
+        }
+        public CommandSeekBuilder SetRequestId(long requestid)
+        {
+            _seek.RequestId = (ulong)requestid;
+            return new CommandSeekBuilder(_seek);
+        }
+        public CommandSeekBuilder SetMessageId(long ledgerid, long entryId)
+        {
+            var messageid = new MessageIdDataBuilder()
+                .SetLedgerId(ledgerid)
+                .SetEntryId(entryId).Build();
+            _seek.MessageId = messageid;
+            return new CommandSeekBuilder(_seek);
+        }
+        public CommandSeekBuilder SetMessagePublishTime(long messagePublishTime)
+        {
+            _seek.MessagePublishTime = (ulong)messagePublishTime;
+            return new CommandSeekBuilder(_seek);
+        }
+        public CommandSeek Build()
+        {
+            return _seek;
+        }
+    }
+}
