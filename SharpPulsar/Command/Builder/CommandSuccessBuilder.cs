c3b3e209094f65b344617eb8640d19f18310ee80
eabanonu <eabanonu@yahoo.com>
2020-01-20 12:17:06 +0100

    CommandSuccessBuilder

---
+++ SharpPulsar/Command/Builder/CommandSuccessBuilder.cs
@@ -0,0 +1,26 @@
+﻿using SharpPulsar.Common.PulsarApi;
+
+namespace SharpPulsar.Command.Builder
+{
+    public class CommandSuccessBuilder
+    {
+        private static CommandSuccess _success;
+        public CommandSuccessBuilder()
+        {
+            _success = new CommandSuccess();
+        }
+        private CommandSuccessBuilder(CommandSuccess success)
+        {
+            _success = success;
+        }
+        public static CommandSuccessBuilder SetRequestId(long requestId)
+        {
+            _success.RequestId = (ulong)requestId;
+            return new CommandSuccessBuilder(_success);
+        }
+        public static CommandSuccess Build()
+        {
+            return _success;
+        }
+    }
+}
