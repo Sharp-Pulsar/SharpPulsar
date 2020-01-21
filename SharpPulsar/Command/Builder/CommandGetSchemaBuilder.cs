961189a3db154c4e818ad14409517a62d738947d
eabanonu <eabanonu@yahoo.com>
2020-01-21 11:08:13 +0100

    CommandGetSchemaBuilder

---
+++ SharpPulsar/Command/Builder/CommandGetSchemaBuilder.cs
@@ -0,0 +1,37 @@
+﻿using SharpPulsar.Common.Protocol.Schema;
+using SharpPulsar.Common.PulsarApi;
+
+namespace SharpPulsar.Command.Builder
+{
+    public class CommandGetSchemaBuilder
+    {
+        private CommandGetSchema _schema;
+        public CommandGetSchemaBuilder()
+        {
+            _schema = new CommandGetSchema();
+        }
+        private CommandGetSchemaBuilder(CommandGetSchema schema)
+        {
+            _schema = schema;
+        }
+        public CommandGetSchemaBuilder SetRequestId(long requestId)
+        {
+            _schema.RequestId = (ulong)requestId;
+            return new CommandGetSchemaBuilder(_schema);
+        }
+        public CommandGetSchemaBuilder SetSchemaVersion(SchemaVersion version)
+        {
+            _schema.SchemaVersion = (byte[])(object)version.Bytes();
+            return new CommandGetSchemaBuilder(_schema);
+        }
+        public CommandGetSchemaBuilder SetTopic(string topic)
+        {
+            _schema.Topic = topic;
+            return new CommandGetSchemaBuilder(_schema);
+        }
+        public CommandGetSchema Build()
+        {
+            return _schema;
+        }
+    }
+}
