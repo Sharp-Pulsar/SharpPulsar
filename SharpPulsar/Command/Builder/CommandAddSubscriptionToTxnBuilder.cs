f24c77e26fb06af50eb5e783be3427999a41b73f
eabanonu <eabanonu@yahoo.com>
2020-01-21 11:35:34 +0100

    CommandAddSubscriptionToTxnBuilder

---
+++ SharpPulsar/Command/Builder/CommandAddSubscriptionToTxnBuilder.cs
@@ -0,0 +1,42 @@
+﻿using SharpPulsar.Common.PulsarApi;
+using System.Collections.Generic;
+
+namespace SharpPulsar.Command.Builder
+{
+    public class CommandAddSubscriptionToTxnBuilder
+    {
+        private readonly CommandAddSubscriptionToTxn _txn;
+        public CommandAddSubscriptionToTxnBuilder()
+        {
+            _txn = new CommandAddSubscriptionToTxn();
+        }
+        private CommandAddSubscriptionToTxnBuilder(CommandAddSubscriptionToTxn txn)
+        {
+            _txn = txn;
+        }
+        public CommandAddSubscriptionToTxnBuilder SetRequestId(long requestId)
+        {
+            _txn.RequestId = (ulong)requestId;
+            return new CommandAddSubscriptionToTxnBuilder(_txn);
+        }
+        public CommandAddSubscriptionToTxnBuilder AddSubscriptions(IList<Subscription> subscription)
+        {
+            _txn.Subscriptions.AddRange(subscription);
+            return new CommandAddSubscriptionToTxnBuilder(_txn);
+        }
+        public CommandAddSubscriptionToTxnBuilder SetTxnidLeastBits(long txnidLeastBits)
+        {
+            _txn.TxnidLeastBits = (ulong)txnidLeastBits;
+            return new CommandAddSubscriptionToTxnBuilder(_txn);
+        }
+        public CommandAddSubscriptionToTxnBuilder SetTxnidMostBits(long txnidMostBits)
+        {
+            _txn.TxnidMostBits = (ulong)txnidMostBits;
+            return new CommandAddSubscriptionToTxnBuilder(_txn);
+        }
+        public CommandAddSubscriptionToTxn Build()
+        {
+            return _txn;
+        }
+    }
+}
