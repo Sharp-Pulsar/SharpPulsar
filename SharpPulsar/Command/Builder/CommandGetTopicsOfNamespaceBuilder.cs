c00c7fb7d6925b129c5431769fae1d3a1c93a7f5
eabanonu <eabanonu@yahoo.com>
2020-01-21 10:52:53 +0100

    CommandGetTopicsOfNamespaceBuilder

---
+++ SharpPulsar/Command/Builder/CommandGetTopicsOfNamespaceBuilder.cs
@@ -0,0 +1,37 @@
+﻿using SharpPulsar.Common.PulsarApi;
+using static SharpPulsar.Common.PulsarApi.CommandGetTopicsOfNamespace;
+
+namespace SharpPulsar.Command.Builder
+{
+    public class CommandGetTopicsOfNamespaceBuilder
+    {
+        private CommandGetTopicsOfNamespace _namespace;
+        public CommandGetTopicsOfNamespaceBuilder()
+        {
+            _namespace = new CommandGetTopicsOfNamespace();
+        }
+        private CommandGetTopicsOfNamespaceBuilder(CommandGetTopicsOfNamespace @namespace)
+        {
+            _namespace = @namespace;
+        }
+        public CommandGetTopicsOfNamespaceBuilder SetMode(Mode mode)
+        {
+            _namespace.mode = mode;
+            return new CommandGetTopicsOfNamespaceBuilder(_namespace);
+        }
+        public CommandGetTopicsOfNamespaceBuilder SetNamespace(string @namespace)
+        {
+            _namespace.Namespace = @namespace;
+            return new CommandGetTopicsOfNamespaceBuilder(_namespace);
+        }
+        public CommandGetTopicsOfNamespaceBuilder SetRequestId(long requestid)
+        {
+            _namespace.RequestId = (ulong)requestid;
+            return new CommandGetTopicsOfNamespaceBuilder(_namespace);
+        }
+        public CommandGetTopicsOfNamespace Build()
+        {
+            return _namespace;
+        }
+    }
+}
