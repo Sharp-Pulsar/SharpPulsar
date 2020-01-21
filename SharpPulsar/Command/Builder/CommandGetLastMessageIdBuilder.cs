b55be5c991c6d7af463e583a8a886b04f61911d3
eabanonu <eabanonu@yahoo.com>
2020-01-21 11:00:55 +0100

    CommandGetLastMessageIdBuilder

---
+++ SharpPulsar/Command/Builder/CommandGetLastMessageIdBuilder.cs
@@ -0,0 +1,31 @@
+﻿using SharpPulsar.Common.PulsarApi;
+
+namespace SharpPulsar.Command.Builder
+{
+    public class CommandGetLastMessageIdBuilder
+    {
+        private readonly CommandGetLastMessageId _id;
+        public CommandGetLastMessageIdBuilder()
+        {
+            _id = new CommandGetLastMessageId();
+        }
+        private CommandGetLastMessageIdBuilder(CommandGetLastMessageId id)
+        {
+            _id = id;
+        }
+        public CommandGetLastMessageIdBuilder SetConsumerId(long consumerId)
+        {
+            _id.ConsumerId = (ulong)consumerId;
+            return new CommandGetLastMessageIdBuilder(_id);
+        }
+        public CommandGetLastMessageIdBuilder SetRequestId(long requestId)
+        {
+            _id.RequestId = (ulong)requestId;
+            return new CommandGetLastMessageIdBuilder(_id);
+        }
+        public CommandGetLastMessageId Build()
+        {
+            return _id;
+        }
+    }
+}
