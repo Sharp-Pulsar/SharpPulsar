c456106c483d0092714021ad609390e08b930d2b
eabanonu <eabanonu@yahoo.com>
2020-01-21 10:32:31 +0100

    CommandFlowBuilder

---
+++ SharpPulsar/Command/Builder/CommandFlowBuilder.cs
@@ -0,0 +1,31 @@
+﻿using SharpPulsar.Common.PulsarApi;
+
+namespace SharpPulsar.Command.Builder
+{
+    public class CommandFlowBuilder
+    {
+        private readonly CommandFlow _flow;
+        public CommandFlowBuilder()
+        {
+            _flow = new CommandFlow();
+        }
+        private CommandFlowBuilder(CommandFlow ack)
+        {
+            _flow = ack;
+        }
+        public CommandFlowBuilder SetConsumerId(long consumerId)
+        {
+            _flow.ConsumerId = (ulong)consumerId;
+            return new CommandFlowBuilder(_flow);
+        }
+        public CommandFlowBuilder SetMessagePermits(int messagePermits)
+        {
+            _flow.messagePermits = (uint)messagePermits;
+            return new CommandFlowBuilder(_flow);
+        }
+        public CommandFlow Build()
+        {
+            return _flow;
+        }
+    }
+}
