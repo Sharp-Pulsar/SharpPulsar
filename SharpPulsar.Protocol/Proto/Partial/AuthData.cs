0e9de7d923b3df44097f715a4e03e8e495a4dfb0
eabanonu <eabanonu@yahoo.com>
2020-02-01 13:02:17 +0100

    CommandAuthChallenge

---
+++ SharpPulsar.Protocol/Proto/Partial/AuthData.cs
@@ -0,0 +1,352 @@
+﻿using DotNetty.Common;
+using Google.Protobuf;
+using SharpPulsar.Util.Protobuf;
+using System;
+using static SharpPulsar.Util.Protobuf.ByteBufCodedInputStream;
+
+namespace SharpPulsar.Protocol.Proto
+{
+	public sealed partial class AuthData : ByteBufCodedOutputStream.ByteBufGeneratedMessage
+	{
+		// Use AuthData.newBuilder() to construct.
+		internal static ThreadLocalPool<AuthData> _pool = new ThreadLocalPool<AuthData>(handle => new AuthData(handle), 1, true);
+
+		internal ThreadLocalPool.Handle _handle;
+		private AuthData(ThreadLocalPool.Handle handle)
+		{
+			_handle = handle;
+		}
+
+		public void Recycle()
+		{
+			InitFields();
+			MemoizedIsInitialized = -1;
+			BitField0_ = 0;
+			MemoizedSerializedSize = -1;
+			if (_handle != null)
+			{
+				_handle.Release(this);
+			}
+		}
+
+		public AuthData(bool NoInit)
+		{
+		}
+
+		internal static readonly AuthData _defaultInstance;
+		public static AuthData DefaultInstance
+		{
+			get
+			{
+				return _defaultInstance;
+			}
+		}
+
+		public AuthData DefaultInstanceForType
+		{
+			get
+			{
+				return _defaultInstance;
+			}
+		}
+
+		internal int BitField0_;
+		
+		public void InitFields()
+		{
+			AuthMethodName = "";
+			AuthData_ = ByteString.Empty;
+		}
+		internal sbyte MemoizedIsInitialized = -1;
+		public bool Initialized
+		{
+			get
+			{
+				sbyte IsInitialized = MemoizedIsInitialized;
+				if (IsInitialized != -1)
+				{
+					return IsInitialized == 1;
+				}
+
+				MemoizedIsInitialized = 1;
+				return true;
+			}
+		}
+
+		public void WriteTo(ByteBufCodedOutputStream Output)
+		{
+			var _ = SerializedSize;
+			if (((BitField0_ & 0x00000001) == 0x00000001))
+			{
+				Output.WriteBytes(1, ByteString.CopyFromUtf8(AuthMethodName));
+			}
+			if (((BitField0_ & 0x00000002) == 0x00000002))
+			{
+				Output.WriteBytes(2, AuthData_);
+			}
+		}
+
+		internal int MemoizedSerializedSize = -1;
+		public int SerializedSize => CalculateSize();
+
+		internal const long SerialVersionUID = 0L;
+		
+		public static Builder NewBuilder()
+		{
+			return Builder.Create();
+		}
+		public Builder NewBuilderForType()
+		{
+			return NewBuilder();
+		}
+		public static Builder NewBuilder(AuthData Prototype)
+		{
+			return NewBuilder().MergeFrom(Prototype);
+		}
+		public Builder ToBuilder()
+		{
+			return NewBuilder(this);
+		}
+
+		public sealed class Builder : ByteBufMessageBuilder
+		{
+			// Construct using org.apache.pulsar.common.api.proto.AuthData.newBuilder()
+			internal static ThreadLocalPool<Builder> _pool = new ThreadLocalPool<Builder>(handle => new Builder(handle), 1, true);
+
+			internal ThreadLocalPool.Handle _handle;
+			private Builder(ThreadLocalPool.Handle handle)
+			{
+				_handle = handle;
+				MaybeForceBuilderInitialization();
+			}
+
+			public void Recycle()
+			{
+				Clear();
+				if (_handle != null)
+				{
+					_handle.Release(this);
+				}
+			}
+
+			public void MaybeForceBuilderInitialization()
+			{
+			}
+			internal static Builder Create()
+			{
+				return _pool.Take();
+			}
+
+			public Builder Clear()
+			{
+				AuthMethodName_ = "";
+				BitField0_ = (BitField0_ & ~0x00000001);
+				AuthData_ = ByteString.Empty;
+				BitField0_ = (BitField0_ & ~0x00000002);
+				return this;
+			}
+
+			public Builder Clone()
+			{
+				return Create().MergeFrom(BuildPartial());
+			}
+
+			public AuthData DefaultInstanceForType
+			{
+				get
+				{
+					return Proto.AuthData.DefaultInstance;
+				}
+			}
+
+			public AuthData Build()
+			{
+				AuthData Result = BuildPartial();
+				if (!Result.Initialized)
+				{
+					throw new NullReferenceException($"{Result.GetType().Name} not initialized");
+				}
+				return Result;
+			}
+
+			public AuthData BuildParsed()
+			{
+				AuthData Result = BuildPartial();
+				if (!Result.Initialized)
+				{
+					throw new NullReferenceException($"{Result.GetType().Name} not initialized");
+				}
+				return Result;
+			}
+
+			public AuthData BuildPartial()
+			{
+				AuthData Result = Proto.AuthData._pool.Take();
+				int FromBitField0_ = BitField0_;
+				int ToBitField0_ = 0;
+				if (((FromBitField0_ & 0x00000001) == 0x00000001))
+				{
+					ToBitField0_ |= 0x00000001;
+				}
+				Result.AuthMethodName = AuthMethodName_.ToString();
+				if (((FromBitField0_ & 0x00000002) == 0x00000002))
+				{
+					ToBitField0_ |= 0x00000002;
+				}
+				Result.AuthData_ = AuthData_;
+				Result.BitField0_ = ToBitField0_;
+				return Result;
+			}
+
+			public Builder MergeFrom(AuthData Other)
+			{
+				if (Other == DefaultInstance)
+				{
+					return this;
+				}
+				if (Other.HasAuthMethodName)
+				{
+					SetAuthMethodName(Other.AuthMethodName);
+				}
+				if (Other.HasAuthData_)
+				{
+					SetAuthData(Other.AuthData_);
+				}
+				return this;
+			}
+
+			public bool Initialized
+			{
+				get
+				{
+					return true;
+				}
+			}
+
+			public ByteBufMessageBuilder MergeFrom(ByteBufCodedInputStream input, ExtensionRegistry extensionRegistry)
+			{
+				while (true)
+				{
+					int Tag = input.ReadTag();
+					switch (Tag)
+					{
+						case 0:
+
+							return this;
+						default:
+							{
+								if (!input.SkipField(Tag))
+								{
+
+									return this;
+								}
+								break;
+							}
+						case 10:
+							{
+								BitField0_ |= 0x00000001;
+								AuthMethodName_ = input.ReadBytes();
+								break;
+							}
+						case 18:
+							{
+								BitField0_ |= 0x00000002;
+								AuthData_ = input.ReadBytes();
+								break;
+							}
+					}
+				}
+			}
+
+			internal int BitField0_;
+
+			// optional string auth_method_name = 1;
+			internal object AuthMethodName_ = "";
+			public bool HasAuthMethodName()
+			{
+				return ((BitField0_ & 0x00000001) == 0x00000001);
+			}
+			public string getAuthMethodName()
+			{
+				object Ref = AuthMethodName_;
+				if (!(Ref is string))
+				{
+					string S = ((ByteString)Ref).ToStringUtf8();
+					AuthMethodName_ = S;
+					return S;
+				}
+				else
+				{
+					return (string)Ref;
+				}
+			}
+			public Builder SetAuthMethodName(string Value)
+			{
+				if (string.ReferenceEquals(Value, null))
+				{
+					throw new System.NullReferenceException();
+				}
+				BitField0_ |= 0x00000001;
+				AuthMethodName_ = Value;
+
+				return this;
+			}
+			public Builder ClearAuthMethodName()
+			{
+				BitField0_ = (BitField0_ & ~0x00000001);
+				AuthMethodName_ = DefaultInstance.AuthMethodName;
+
+				return this;
+			}
+			public void SetAuthMethodName(ByteString Value)
+			{
+				BitField0_ |= 0x00000001;
+				AuthMethodName_ = Value;
+
+			}
+
+			// optional bytes auth_data = 2;
+			internal ByteString AuthData_ = ByteString.Empty;
+			public bool HasAuthData()
+			{
+				return ((BitField0_ & 0x00000002) == 0x00000002);
+			}
+			public ByteString AuthData
+			{
+				get
+				{
+					return AuthData_;
+				}
+			}
+			public Builder SetAuthData(ByteString Value)
+			{
+				if (Value == null)
+				{
+					throw new System.NullReferenceException();
+				}
+				BitField0_ |= 0x00000002;
+				AuthData_ = Value;
+
+				return this;
+			}
+			public Builder ClearAuthData()
+			{
+				BitField0_ = (BitField0_ & ~0x00000002);
+				AuthData_ = DefaultInstance.AuthData_;
+
+				return this;
+			}
+
+			// @@protoc_insertion_point(builder_scope:pulsar.proto.AuthData)
+		}
+
+		static AuthData()
+		{
+			_defaultInstance = new AuthData(true);
+			_defaultInstance.InitFields();
+		}
+
+		// @@protoc_insertion_point(class_scope:pulsar.proto.AuthData)
+	}
+
+}
---
+++ SharpPulsar.Protocol/Proto/Partial/CommandAuthChallenge.cs
@@ -0,0 +1,426 @@
+﻿using DotNetty.Common;
+using Google.Protobuf;
+using SharpPulsar.Util.Protobuf;
+using System;
+using static SharpPulsar.Util.Protobuf.ByteBufCodedInputStream;
+
+namespace SharpPulsar.Protocol.Proto
+{
+	public sealed partial class CommandAuthChallenge : ByteBufCodedOutputStream.ByteBufGeneratedMessage
+	{
+		// Use CommandAuthChallenge.newBuilder() to construct.
+		internal static ThreadLocalPool<CommandAuthChallenge> _pool = new ThreadLocalPool<CommandAuthChallenge>(handle => new CommandAuthChallenge(handle), 1, true);
+
+		internal ThreadLocalPool.Handle _handle;
+		private CommandAuthChallenge(ThreadLocalPool.Handle handle)
+		{
+			_handle = handle;
+		}
+
+		public void Recycle()
+		{
+			InitFields();
+			MemoizedIsInitialized = -1;
+			_hasBits0 = 0;
+			MemoizedSerializedSize = -1;
+			if (_handle != null)
+			{
+				_handle.Release(this);
+			}
+		}
+
+		public CommandAuthChallenge(bool NoInit)
+		{
+		}
+
+
+		internal static readonly CommandAuthChallenge _defaultInstance;
+		public static CommandAuthChallenge DefaultInstance
+		{
+			get
+			{
+				return _defaultInstance;
+			}
+		}
+
+		public CommandAuthChallenge DefaultInstanceForType
+		{
+			get
+			{
+				return _defaultInstance;
+			}
+		}
+
+		
+		public void InitFields()
+		{
+			ServerVersion = "";
+			Challenge = AuthData.DefaultInstance;
+			ProtocolVersion = 0;
+		}
+		internal sbyte MemoizedIsInitialized = -1;
+		public bool Initialized
+		{
+			get
+			{
+				sbyte IsInitialized = MemoizedIsInitialized;
+				if (IsInitialized != -1)
+				{
+					return IsInitialized == 1;
+				}
+
+				MemoizedIsInitialized = 1;
+				return true;
+			}
+		}
+
+		
+		public void WriteTo(ByteBufCodedOutputStream Output)
+		{
+			var _ = SerializedSize;
+			if (((_hasBits0 & 0x00000001) == 0x00000001))
+			{
+				Output.WriteBytes(1, ByteString.CopyFromUtf8(ServerVersion));
+			}
+			if (((_hasBits0 & 0x00000002) == 0x00000002))
+			{
+				Output.WriteMessage(2, Challenge);
+			}
+			if (((_hasBits0 & 0x00000004) == 0x00000004))
+			{
+				Output.WriteInt32(3, ProtocolVersion);
+			}
+		}
+
+		internal int MemoizedSerializedSize = -1;
+		public int SerializedSize => CalculateSize();
+
+		internal const long SerialVersionUID = 0L;
+		
+		public static Builder NewBuilder()
+		{
+			return Builder.Create();
+		}
+		public Builder NewBuilderForType()
+		{
+			return NewBuilder();
+		}
+		public static Builder NewBuilder(CommandAuthChallenge Prototype)
+		{
+			return NewBuilder().MergeFrom(Prototype);
+		}
+		public Builder ToBuilder()
+		{
+			return NewBuilder(this);
+		}
+
+		public sealed class Builder : ByteBufMessageBuilder
+		{
+			// Construct using org.apache.pulsar.common.api.proto.CommandAuthChallenge.newBuilder()
+			internal static ThreadLocalPool<Builder> _pool = new ThreadLocalPool<Builder>(handle => new Builder(handle), 1, true);
+
+			internal ThreadLocalPool.Handle _handle;
+			private Builder(ThreadLocalPool.Handle handle)
+			{
+				_handle = handle;
+				MaybeForceBuilderInitialization();
+			}
+
+			public void Recycle()
+			{
+				Clear();
+				if (_handle != null)
+				{
+					_handle.Release(this);
+				}
+			}
+
+			public void MaybeForceBuilderInitialization()
+			{
+			}
+			internal static Builder Create()
+			{
+				return _pool.Take();
+			}
+
+			public Builder Clear()
+			{
+				ServerVersion_ = "";
+				BitField0_ = (BitField0_ & ~0x00000001);
+				Challenge_ = AuthData.DefaultInstance;
+				BitField0_ = (BitField0_ & ~0x00000002);
+				ProtocolVersion_ = 0;
+				BitField0_ = (BitField0_ & ~0x00000004);
+				return this;
+			}
+
+			public Builder Clone()
+			{
+				return Create().MergeFrom(BuildPartial());
+			}
+
+			public CommandAuthChallenge DefaultInstanceForType
+			{
+				get
+				{
+					return DefaultInstance;
+				}
+			}
+
+			public CommandAuthChallenge Build()
+			{
+				CommandAuthChallenge Result = BuildPartial();
+				if (!Result.Initialized)
+				{
+					throw new NullReferenceException($"{Result.GetType().Name} not initialized");
+				}
+				return Result;
+			}
+
+			public CommandAuthChallenge BuildParsed()
+			{
+				CommandAuthChallenge Result = BuildPartial();
+				if (!Result.Initialized)
+				{
+					throw new NullReferenceException($"{Result.GetType().Name} not initialized");
+				}
+				return Result;
+			}
+
+			public CommandAuthChallenge BuildPartial()
+			{
+				CommandAuthChallenge Result = CommandAuthChallenge._pool.Take();
+				int FromBitField0_ = BitField0_;
+				int ToBitField0_ = 0;
+				if (((FromBitField0_ & 0x00000001) == 0x00000001))
+				{
+					ToBitField0_ |= 0x00000001;
+				}
+				Result.ServerVersion = ServerVersion_.ToString();
+				if (((FromBitField0_ & 0x00000002) == 0x00000002))
+				{
+					ToBitField0_ |= 0x00000002;
+				}
+				Result.Challenge = Challenge_;
+				if (((FromBitField0_ & 0x00000004) == 0x00000004))
+				{
+					ToBitField0_ |= 0x00000004;
+				}
+				Result.ProtocolVersion = ProtocolVersion_;
+				Result._hasBits0 = ToBitField0_;
+				return Result;
+			}
+
+			public Builder MergeFrom(CommandAuthChallenge Other)
+			{
+				if (Other == DefaultInstance)
+				{
+					return this;
+				}
+				if (Other.HasServerVersion)
+				{
+					SetServerVersion(Other.ServerVersion);
+				}
+				if (Other.HasChallenge)
+				{
+					MergeChallenge(Other.Challenge);
+				}
+				if (Other.HasProtocolVersion)
+				{
+					SetProtocolVersion(Other.ProtocolVersion);
+				}
+				return this;
+			}
+
+			public bool Initialized
+			{
+				get
+				{
+					return true;
+				}
+			}
+			public ByteBufMessageBuilder MergeFrom(ByteBufCodedInputStream input, ExtensionRegistry extensionRegistry)
+			{
+				while (true)
+				{
+					int Tag = input.ReadTag();
+					switch (Tag)
+					{
+						case 0:
+
+							return this;
+						default:
+							{
+								if (!input.SkipField(Tag))
+								{
+
+									return this;
+								}
+								break;
+							}
+						case 10:
+							{
+								BitField0_ |= 0x00000001;
+								ServerVersion_ = input.ReadBytes();
+								break;
+							}
+						case 18:
+							{
+								AuthData.Builder SubBuilder = AuthData.NewBuilder();
+								if (HasChallenge())
+								{
+									SubBuilder.MergeFrom(GetChallenge());
+								}
+								input.ReadMessage(SubBuilder, extensionRegistry);
+								SetChallenge(SubBuilder.BuildPartial());
+								SubBuilder.Recycle();
+								break;
+							}
+						case 24:
+							{
+								BitField0_ |= 0x00000004;
+								ProtocolVersion_ = input.ReadInt32();
+								break;
+							}
+					}
+				}
+			}
+
+			internal int BitField0_;
+
+			// optional string server_version = 1;
+			internal object ServerVersion_ = "";
+			public bool HasServerVersion()
+			{
+				return ((BitField0_ & 0x00000001) == 0x00000001);
+			}
+			public string getServerVersion()
+			{
+				object Ref = ServerVersion_;
+				if (!(Ref is string))
+				{
+					string S = ((ByteString)Ref).ToStringUtf8();
+					ServerVersion_ = S;
+					return S;
+				}
+				else
+				{
+					return (string)Ref;
+				}
+			}
+			public Builder SetServerVersion(string Value)
+			{
+				if (string.ReferenceEquals(Value, null))
+				{
+					throw new System.NullReferenceException();
+				}
+				BitField0_ |= 0x00000001;
+				ServerVersion_ = Value;
+
+				return this;
+			}
+			public Builder ClearServerVersion()
+			{
+				BitField0_ = (BitField0_ & ~0x00000001);
+				ServerVersion_ = DefaultInstance.ServerVersion;
+
+				return this;
+			}
+			public void SetServerVersion(ByteString Value)
+			{
+				BitField0_ |= 0x00000001;
+				ServerVersion_ = Value;
+
+			}
+
+			// optional .pulsar.proto.AuthData challenge = 2;
+			internal AuthData Challenge_ = AuthData.DefaultInstance;
+			public bool HasChallenge()
+			{
+				return ((BitField0_ & 0x00000002) == 0x00000002);
+			}
+			public AuthData GetChallenge()
+			{
+				return Challenge_;
+			}
+			public Builder SetChallenge(AuthData Value)
+			{
+				if (Value == null)
+				{
+					throw new System.NullReferenceException();
+				}
+				Challenge_ = Value;
+
+				BitField0_ |= 0x00000002;
+				return this;
+			}
+			public Builder SetChallenge(AuthData.Builder BuilderForValue)
+			{
+				Challenge_ = BuilderForValue.Build();
+
+				BitField0_ |= 0x00000002;
+				return this;
+			}
+			public Builder MergeChallenge(AuthData Value)
+			{
+				if (((BitField0_ & 0x00000002) == 0x00000002) && Challenge_ != AuthData.DefaultInstance)
+				{
+					Challenge_ = AuthData.NewBuilder(Challenge_).MergeFrom(Value).BuildPartial();
+				}
+				else
+				{
+					Challenge_ = Value;
+				}
+
+				BitField0_ |= 0x00000002;
+				return this;
+			}
+			public Builder ClearChallenge()
+			{
+				Challenge_ = AuthData.DefaultInstance;
+
+				BitField0_ = (BitField0_ & ~0x00000002);
+				return this;
+			}
+
+			// optional int32 protocol_version = 3 [default = 0];
+			internal int ProtocolVersion_;
+			public bool HasProtocolVersion()
+			{
+				return ((BitField0_ & 0x00000004) == 0x00000004);
+			}
+			public int ProtocolVersion
+			{
+				get
+				{
+					return ProtocolVersion_;
+				}
+			}
+			public Builder SetProtocolVersion(int Value)
+			{
+				BitField0_ |= 0x00000004;
+				ProtocolVersion_ = Value;
+
+				return this;
+			}
+			public Builder ClearProtocolVersion()
+			{
+				BitField0_ = (BitField0_ & ~0x00000004);
+				ProtocolVersion_ = 0;
+
+				return this;
+			}
+
+			// @@protoc_insertion_point(builder_scope:pulsar.proto.CommandAuthChallenge)
+		}
+
+		static CommandAuthChallenge()
+		{
+			_defaultInstance = new CommandAuthChallenge(true);
+			_defaultInstance.InitFields();
+		}
+
+		// @@protoc_insertion_point(class_scope:pulsar.proto.CommandAuthChallenge)
+	}
+
+}
