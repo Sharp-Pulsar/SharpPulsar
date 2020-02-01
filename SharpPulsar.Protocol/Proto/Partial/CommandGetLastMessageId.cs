7a22d7cf09307ad5b5fd3f1853d38fbf40dae078
eabanonu <eabanonu@yahoo.com>
2020-02-01 10:45:53 +0100

    CommandGetLastMessageId

---
+++ SharpPulsar.Protocol/Proto/Partial/CommandGetLastMessageId.cs
@@ -0,0 +1,351 @@
+﻿using DotNetty.Common;
+using Google.Protobuf;
+using SharpPulsar.Util.Protobuf;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static SharpPulsar.Util.Protobuf.ByteBufCodedInputStream;
+
+namespace SharpPulsar.Protocol.Proto
+{
+	public sealed partial class CommandGetLastMessageId : ByteBufCodedOutputStream.ByteBufGeneratedMessage
+	{
+		// Use CommandGetLastMessageId.newBuilder() to construct.
+		internal static ThreadLocalPool<CommandGetLastMessageId> _pool = new ThreadLocalPool<CommandGetLastMessageId>(handle => new CommandGetLastMessageId(handle), 1, true);
+
+		internal ThreadLocalPool.Handle _handle;
+		private CommandGetLastMessageId(ThreadLocalPool.Handle handle)
+		{
+			_handle = handle;
+		}
+		public void Recycle()
+		{
+			InitFields();
+			MemoizedIsInitialized = -1;
+			_hasBits0 = 0;
+			MemoizedSerializedSize = -1;
+			if (_handle != null)
+			{
+				_handle.Release(this);
+			}
+		}
+
+		public CommandGetLastMessageId(bool NoInit)
+		{
+		}
+
+
+		internal static readonly CommandGetLastMessageId _defaultInstance;
+		public static CommandGetLastMessageId DefaultInstance
+		{
+			get
+			{
+				return _defaultInstance;
+			}
+		}
+
+		public CommandGetLastMessageId DefaultInstanceForType
+		{
+			get
+			{
+				return _defaultInstance;
+			}
+		}
+
+		
+		public void InitFields()
+		{
+			ConsumerId = 0L;
+			RequestId = 0L;
+		}
+		internal sbyte MemoizedIsInitialized = -1;
+		public bool Initialized
+		{
+			get
+			{
+				sbyte IsInitialized = MemoizedIsInitialized;
+				if (IsInitialized != -1)
+				{
+					return IsInitialized == 1;
+				}
+
+				if (!HasConsumerId)
+				{
+					MemoizedIsInitialized = 0;
+					return false;
+				}
+				if (!HasRequestId)
+				{
+					MemoizedIsInitialized = 0;
+					return false;
+				}
+				MemoizedIsInitialized = 1;
+				return true;
+			}
+		}
+
+		public void WriteTo(ByteBufCodedOutputStream Output)
+		{
+			var _ = SerializedSize;
+			if (((_hasBits0 & 0x00000001) == 0x00000001))
+			{
+				Output.WriteUInt64(1, (long)ConsumerId);
+			}
+			if (((_hasBits0 & 0x00000002) == 0x00000002))
+			{
+				Output.WriteUInt64(2, (long)RequestId);
+			}
+		}
+
+		internal int MemoizedSerializedSize = -1;
+		public int SerializedSize => CalculateSize();
+
+		internal const long SerialVersionUID = 0L;
+		
+		public static Builder NewBuilder()
+		{
+			return Builder.Create();
+		}
+		public Builder NewBuilderForType()
+		{
+			return NewBuilder();
+		}
+		public static Builder NewBuilder(CommandGetLastMessageId Prototype)
+		{
+			return NewBuilder().MergeFrom(Prototype);
+		}
+		public Builder ToBuilder()
+		{
+			return NewBuilder(this);
+		}
+
+		public sealed class Builder : ByteBufMessageBuilder
+		{
+			// Construct using org.apache.pulsar.common.api.proto.CommandGetLastMessageId.newBuilder()
+			internal static ThreadLocalPool<Builder> _pool = new ThreadLocalPool<Builder>(handle => new Builder(handle), 1, true);
+
+			internal ThreadLocalPool.Handle _handle;
+			private Builder(ThreadLocalPool.Handle handle)
+			{
+				_handle = handle;
+				MaybeForceBuilderInitialization();
+			}
+			public void Recycle()
+			{
+				Clear();
+				if (_handle != null)
+				{
+					_handle.Release(this);
+				}
+			}
+
+			public void MaybeForceBuilderInitialization()
+			{
+			}
+			internal static Builder Create()
+			{
+				return _pool.Take();
+			}
+
+			public Builder Clear()
+			{
+				ConsumerId_ = 0L;
+				BitField0_ = (BitField0_ & ~0x00000001);
+				RequestId_ = 0L;
+				BitField0_ = (BitField0_ & ~0x00000002);
+				return this;
+			}
+
+			public Builder Clone()
+			{
+				return Create().MergeFrom(BuildPartial());
+			}
+
+			public CommandGetLastMessageId DefaultInstanceForType
+			{
+				get
+				{
+					return DefaultInstance;
+				}
+			}
+
+			public CommandGetLastMessageId Build()
+			{
+				CommandGetLastMessageId Result = BuildPartial();
+				if (!Result.Initialized)
+				{
+					throw new NullReferenceException($"{Result.GetType().Name} not initialized");
+				}
+				return Result;
+			}
+						
+			public CommandGetLastMessageId BuildParsed()
+			{
+				CommandGetLastMessageId Result = BuildPartial();
+				if (!Result.Initialized)
+				{
+					throw new NullReferenceException($"{Result.GetType().Name} not initialized");
+				}
+				return Result;
+			}
+
+			public CommandGetLastMessageId BuildPartial()
+			{
+				CommandGetLastMessageId Result = CommandGetLastMessageId._pool.Take();
+				int FromBitField0_ = BitField0_;
+				int ToBitField0_ = 0;
+				if (((FromBitField0_ & 0x00000001) == 0x00000001))
+				{
+					ToBitField0_ |= 0x00000001;
+				}
+				Result.ConsumerId = (ulong)ConsumerId_;
+				if (((FromBitField0_ & 0x00000002) == 0x00000002))
+				{
+					ToBitField0_ |= 0x00000002;
+				}
+				Result.RequestId = (ulong)RequestId_;
+				Result._hasBits0 = ToBitField0_;
+				return Result;
+			}
+
+			public Builder MergeFrom(CommandGetLastMessageId Other)
+			{
+				if (Other == DefaultInstance)
+				{
+					return this;
+				}
+				if (Other.HasConsumerId)
+				{
+					SetConsumerId((long)Other.ConsumerId);
+				}
+				if (Other.HasRequestId)
+				{
+					SetRequestId((long)Other.RequestId);
+				}
+				return this;
+			}
+
+			public bool Initialized
+			{
+				get
+				{
+					if (!HasConsumerId())
+					{
+
+						return false;
+					}
+					if (!HasRequestId())
+					{
+
+						return false;
+					}
+					return true;
+				}
+			}
+
+			public ByteBufMessageBuilder MergeFrom(ByteBufCodedInputStream input, ExtensionRegistry extensionRegistry)
+			{
+				while (true)
+				{
+					int Tag = input.ReadTag();
+					switch (Tag)
+					{
+						case 0:
+
+							return this;
+						default:
+							{
+								if (!input.SkipField(Tag))
+								{
+
+									return this;
+								}
+								break;
+							}
+						case 8:
+							{
+								BitField0_ |= 0x00000001;
+								ConsumerId_ = input.ReadUInt64();
+								break;
+							}
+						case 16:
+							{
+								BitField0_ |= 0x00000002;
+								RequestId_ = input.ReadUInt64();
+								break;
+							}
+					}
+				}
+			}
+
+			internal int BitField0_;
+
+			// required uint64 consumer_id = 1;
+			internal long ConsumerId_;
+			public bool HasConsumerId()
+			{
+				return ((BitField0_ & 0x00000001) == 0x00000001);
+			}
+			public long ConsumerId
+			{
+				get
+				{
+					return ConsumerId_;
+				}
+			}
+			public Builder SetConsumerId(long Value)
+			{
+				BitField0_ |= 0x00000001;
+				ConsumerId_ = Value;
+
+				return this;
+			}
+			public Builder ClearConsumerId()
+			{
+				BitField0_ = (BitField0_ & ~0x00000001);
+				ConsumerId_ = 0L;
+
+				return this;
+			}
+
+			// required uint64 request_id = 2;
+			internal long RequestId_;
+			public bool HasRequestId()
+			{
+				return ((BitField0_ & 0x00000002) == 0x00000002);
+			}
+			public long RequestId
+			{
+				get
+				{
+					return RequestId_;
+				}
+			}
+			public Builder SetRequestId(long Value)
+			{
+				BitField0_ |= 0x00000002;
+				RequestId_ = Value;
+
+				return this;
+			}
+			public Builder ClearRequestId()
+			{
+				BitField0_ = (BitField0_ & ~0x00000002);
+				RequestId_ = 0L;
+
+				return this;
+			}
+
+			// @@protoc_insertion_point(builder_scope:pulsar.proto.CommandGetLastMessageId)
+		}
+
+		static CommandGetLastMessageId()
+		{
+			_defaultInstance = new CommandGetLastMessageId(true);
+			_defaultInstance.InitFields();
+		}
+
+		// @@protoc_insertion_point(class_scope:pulsar.proto.CommandGetLastMessageId)
+	}
+
+}
