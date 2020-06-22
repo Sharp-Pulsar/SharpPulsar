c307a52b06614b00b66ac1db9ca0088c73d1dc69
eabanonu <eabanonu@yahoo.com>
2020-06-22 14:48:50 +0100

    [WIP] SNI

--- SharpPulsar/Akka/Network/Connector.cs
+++ SharpPulsar/Akka/Network/Connector.cs
@@ -18,10 +18,14 @@
 using System;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Security;
 using System.Net.Sockets;
+using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices;
 using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 using SharpPulsar.Impl.Conf;
 
 namespace SharpPulsar.Akka.Network
@@ -35,7 +39,7 @@ namespace SharpPulsar.Akka.Network
         private readonly bool _verifyCertificateName;
         private readonly bool _encrypt;
         private readonly string _serviceUrl;
-        private string _serviceSniName;
+        private string _targetServerName;
 
         public Connector(ClientConfigurationData conf)
         {
@@ -53,7 +57,7 @@ namespace SharpPulsar.Akka.Network
         public Stream Connect(Uri endPoint, string hostName)
         {
             var host = endPoint.Host;
-            _serviceSniName = hostName;
+            _targetServerName = hostName;
             var stream = GetStream(endPoint);
 
             if (_encrypt)
@@ -69,15 +73,17 @@ namespace SharpPulsar.Akka.Network
             try
             {
                 if (!SniProxy)
-                    tcpClient.Connect(endPoint.Host, endPoint.Port);
-                else
                 {
-                    var proxy = new Uri(_clientConfiguration.ProxyServiceUrl);
-                    tcpClient.Connect(proxy.Host, proxy.Port);
+                    tcpClient.Connect(endPoint.Host, endPoint.Port);
+                    return tcpClient.GetStream();
                 }
-                return tcpClient.GetStream();
+
+                var proxy = new Uri(_clientConfiguration.ProxyServiceUrl);
+                var addresses = Dns.GetHostAddressesAsync(proxy.Host).GetAwaiter().GetResult();
+                var socket = ConnectAsync(addresses, proxy.Port).GetAwaiter().GetResult();
+                return new NetworkStream(socket, true);
             }
-            catch(Exception ex)
+            catch(Exception)
             {
                 tcpClient.Dispose();
                 throw;
@@ -90,7 +96,7 @@ namespace SharpPulsar.Akka.Network
 
             try
             {
-                sslStream = new SslStream(stream, false, ValidateServerCertificate, null);
+                sslStream = new SslStream(stream, true, ValidateServerCertificate, null);
                 sslStream.AuthenticateAsClient(host, _clientCertificates, SslProtocols.Tls12, false);
                 return sslStream;
             }
@@ -104,45 +110,122 @@ namespace SharpPulsar.Akka.Network
                 throw;
             }
         }
-        private bool SniProxy => _encrypt && _clientConfiguration.ProxyProtocol != null && !string.IsNullOrWhiteSpace(_clientConfiguration.ProxyServiceUrl);
-
-        private bool ValidateServerCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        //https://github.com/LukeInkster/CSharpCorpus/blob/919b7525a61eb6b475fbcba0d87fd3cb44ef3b38/corefx/src/System.Data.SqlClient/src/System/Data/SqlClient/SNI/SNITcpHandle.cs
+        private static async Task<Socket> ConnectAsync(IPAddress[] serverAddresses, int port)
         {
-            bool result;
-            switch (sslPolicyErrors)
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                case SslPolicyErrors.None:
-                    result = true;
-                    break;
-                case SslPolicyErrors.RemoteCertificateChainErrors:
-
-                    if (_trustedCertificateAuthority is null)
-                        return false;
-
-                    chain.ChainPolicy.ExtraStore.Add(_trustedCertificateAuthority);
-                    _ = chain.Build((X509Certificate2)certificate);
-                    for (var i = 0; i < chain.ChainElements.Count; i++)
-                    {
-                        if (chain.ChainElements[i].Certificate.Thumbprint == _trustedCertificateAuthority.Thumbprint)
-                            return true;
-                    }
-                    return false;
-                case SslPolicyErrors.RemoteCertificateNameMismatch:
-                    var cert = new X509Certificate2(certificate);
-                    var cn = cert.GetNameInfo(X509NameType.SimpleName, false);
-                    var cleanName = cn?.Substring(cn.LastIndexOf('*') + 1);
-                    string[] addresses = { _serviceUrl, _serviceSniName };
-
-                    // if the ending of the sni and servername do match the common name of the cert, fail
-                    result = addresses.Count(item => cleanName != null && item.EndsWith(cleanName)) == addresses.Count();
-                    break;
-
-                default:
-                    result = false;
-                    break;
+                var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                await socket.ConnectAsync(serverAddresses, port).ConfigureAwait(false);
+                return socket;
             }
 
-            return result;
+            // On unix we can't use the instance Socket methods that take multiple endpoints
+
+            if (serverAddresses == null)
+            {
+                throw new ArgumentNullException(nameof(serverAddresses));
+            }
+            if (serverAddresses.Length == 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(serverAddresses));
+            }
+
+            // Try each address in turn, and return the socket opened for the first one that works.
+            ExceptionDispatchInfo lastException = null;
+            foreach (IPAddress address in serverAddresses)
+            {
+                var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                try
+                {
+                    await socket.ConnectAsync(address, port).ConfigureAwait(false);
+                    return socket;
+                }
+                catch (Exception exc)
+                {
+                    socket.Dispose();
+                    lastException = ExceptionDispatchInfo.Capture(exc);
+                }
+            }
+
+            // Propagate the last failure that occurred
+            if (lastException != null)
+            {
+                lastException.Throw();
+            }
+
+            // Should never get here.  Either there will have been no addresses and we'll have thrown
+            // at the beginning, or one of the addresses will have worked and we'll have returned, or
+            // at least one of the addresses will failed, in which case we will have propagated that.
+            throw new ArgumentException();
+        }
+
+        private bool SniProxy => _encrypt && _clientConfiguration.ProxyProtocol != null && !string.IsNullOrWhiteSpace(_clientConfiguration.ProxyServiceUrl);
+
+        private bool ValidateServerCertificate(object sender, X509Certificate cert, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            if (!_verifyCertificateName)
+                return true;
+
+            if (sslPolicyErrors == SslPolicyErrors.None)
+            {
+                return true;
+            }
+
+            if ((sslPolicyErrors & SslPolicyErrors.RemoteCertificateNameMismatch) != 0)
+            {
+                var certServerName = cert.Subject.Substring(cert.Subject.IndexOf('=') + 1);
+
+                // Verify that target server name matches subject in the certificate
+                if (_targetServerName.Length > certServerName.Length)
+                {
+                    return false;
+                }
+                if (_targetServerName.Length == certServerName.Length)
+                {
+                    // Both strings have the same length, so targetServerName must be a FQDN
+                    if (!_targetServerName.Equals(certServerName, StringComparison.OrdinalIgnoreCase))
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (string.Compare(_targetServerName, 0, certServerName, 0, _targetServerName.Length, StringComparison.OrdinalIgnoreCase) != 0)
+                    {
+                        return false;
+                    }
+
+                    // Server name matches cert name for its whole length, so ensure that the
+                    // character following the server name is a '.'. This will avoid
+                    // having server name "ab" match "abc.corp.company.com"
+                    // (Names have different lengths, so the target server can't be a FQDN.)
+                    if (certServerName[_targetServerName.Length] != '.')
+                    {
+                        return false;
+                    }
+                }
+            }
+            else if (sslPolicyErrors == SslPolicyErrors.RemoteCertificateChainErrors)
+            {
+                if (_trustedCertificateAuthority is null)
+                    return false;
+
+                chain.ChainPolicy.ExtraStore.Add(_trustedCertificateAuthority);
+                _ = chain.Build((X509Certificate2)cert);
+                for (var i = 0; i < chain.ChainElements.Count; i++)
+                {
+                    if (chain.ChainElements[i].Certificate.Thumbprint == _trustedCertificateAuthority.Thumbprint)
+                        return true;
+                }
+                return false;
+            }
+            else
+            {
+                // Fail all other SslPolicy cases besides RemoteCertificateNameMismatch
+                return false;
+            }
+            return true;
         }
     }
 }
