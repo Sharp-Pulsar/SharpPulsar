795cd2be17b5dc775a786026da372ae34d4ecf60
Ebere Abanonu <eabanonu@yahoo.com>
2021-02-09 18:41:25 +0100

    Compile

--- build/Build.cs
+++ build/Build.cs
@@ -22,7 +22,7 @@ using static Nuke.Common.Tools.DotNet.DotNetTasks;
     AutoGenerate = true,
     OnPushBranches = new[] { "master", "dev", "beta/*" },
     OnPullRequestBranches = new[] { "beta" },
-    InvokedTargets = new[] { nameof(GitHubActions) })]
+    InvokedTargets = new[] { nameof(Compile) })]
 class Build : NukeBuild
 {
     /// Support plugins are available for:
