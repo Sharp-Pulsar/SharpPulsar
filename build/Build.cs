9ce80e22dcb5b511ff1c647431bad2ff22b3c1aa
Ebere Abanonu <eabanonu@yahoo.com>
2021-02-09 18:41:58 +0100

    Pull

--- build/Build.cs
+++ build/Build.cs
@@ -29,7 +29,7 @@ class Build : NukeBuild
     ///   - JetBrains ReSharper        https://nuke.build/resharper
     ///   - JetBrains Rider            https://nuke.build/rider
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
+    ///   - Microsoft VSCode           https://nuke.build/vscode 
 
     public static int Main () => Execute<Build>(x => x.Compile);
 
