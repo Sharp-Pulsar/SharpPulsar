1153389bbea6f3d6eb43c8c550a4ba154caf886c
Ebere Abanonu <eabanonu@yahoo.com>
2021-02-10 20:06:36 +0100

    standalone

--- build/Build.cs
+++ build/Build.cs
@@ -120,7 +120,7 @@ class Build : NukeBuild
           .SetMount("source=pulsardata,target=/pulsar/data")
           .SetMount("source=pulsarconf,target=/pulsar/conf")
           .SetImage("apachepulsar/pulsar:2.7.0")
-          .SetEntrypoint("bin/pulsar standalone")
+          .SetEntrypoint("standalone")
           );
       });
     Target CheckDockerVersion => _ => _
