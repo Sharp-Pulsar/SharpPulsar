e29193cc12ee95ef1bbcdc519da73d395727e8a4
Ebere Abanonu <eaba@users.noreply.github.com>
2021-02-10 22:31:06 +0100

    Update Build.cs
    Update command

--- build/Build.cs
+++ build/Build.cs
@@ -120,7 +120,7 @@ class Build : NukeBuild
           .SetMount("source=pulsardata,target=/pulsar/data")
           .SetMount("source=pulsarconf,target=/pulsar/conf")
           .SetImage("apachepulsar/pulsar:2.7.0")
-          .SetCommand("/usr/bin/pulsar standalone")
+          .SetCommand("/usr/pulsar/bin/pulsar standalone")
           );
       });
     Target CheckDockerVersion => _ => _
