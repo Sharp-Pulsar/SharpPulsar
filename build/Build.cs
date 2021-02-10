54c70b4a01e06381eac0d9c005cda709df6cb675
Ebere Abanonu <eabanonu@yahoo.com>
2021-02-10 20:57:19 +0100

    Array Args

--- build/Build.cs
+++ build/Build.cs
@@ -113,7 +113,7 @@ class Build : NukeBuild
       {
           DockerTasks.DockerRun(b =>
           b.SetName("pulsar_test")
-          .SetArgs(" -it -p 6650:6650 -p 8080:8080 --mount source=pulsardata,target=/pulsar/data --mount source=pulsarconf,target=/pulsar/conf apachepulsar/pulsar:2.7.0 bin/pulsar standalone")
+          .SetArgs("-it","-p 6650:6650","-p 8080:8080", "--mount source=pulsardata,target=/pulsar/data","--mount source=pulsarconf,target=/pulsar/conf", "apachepulsar/pulsar:2.7.0", "bin/pulsar standalone")
           );
       });
     Target CheckDockerVersion => _ => _
