76762e8ae488c80dbab038fae61cfaffd51187f1
Ebere Abanonu <eabanonu@yahoo.com>
2021-02-13 00:33:05 +0100

    Set Configuration to Debug

--- build/Build.cs
+++ build/Build.cs
@@ -46,7 +46,8 @@ class Build : NukeBuild
     public static int Main () => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+    //readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+    readonly Configuration Configuration = Configuration.Debug;
 
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
