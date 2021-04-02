dd1868aa59da615dfe273a6772c937f421ce9cc8
Ebere Abanonu <eabanonu@yahoo.com>
2021-04-02 19:50:30 +0100

    EventInput

--- SharpPulsar/User/EventSource.cs
+++ SharpPulsar/User/EventSource.cs
@@ -414,4 +414,66 @@ namespace SharpPulsar.User
             }
         }
     }
+    public enum SourceFrom
+    {
+        Sql,
+        Reader
+    }
+    public enum EventMethod
+    {
+        /// <summary>
+        /// EventsByTopicReader is used for retrieving events for a specific topics 
+        /// <para>
+        /// You can retrieve a subset of all events by specifying <paramref name="fromSequenceId"/> and <paramref name="toSequenceId"/>
+        /// or use `0L` and <see cref="long.MaxValue"/> respectively to retrieve all events. Note that
+        /// the corresponding sequence id of each event is provided in the
+        /// <see cref="EventMessage"/>, which makes it possible to resume the
+        /// stream at a later point from a given sequence id.
+        /// </para>
+        /// The returned event stream is ordered by ledgerId and entryId.
+        /// <para>
+        /// The stream is not completed when it reaches the end of the currently stored events,
+        /// but it continues to push new events when new events are persisted.
+        /// Corresponding query that is completed when it reaches the end of the currently
+        /// stored events is provided by CurrentEventsByTopicReader.
+        /// </para>
+        /// </summary>
+        EventsByTopic,
+        /// <summary>
+        /// Same type of query as EventsByTopicReader but the event query
+        /// is completed immediately when it reaches the end of the "result set". Events that are
+        /// stored after the query is completed are not included in the event stream.
+        /// </summary>
+        CurrentEventsByTopic,
+        /// <summary>
+        /// EventsByTagReader is used for retrieving events that were marked with
+        /// a given tag, e.g. all events of an Aggregate Root type.
+        /// To tag events you create an a message with tag key and value as message property.
+        /// Connection is made for each topic in the namespace
+        /// The query is not completed when it reaches the end of the currently stored events,
+        /// but it continues to push new events when new events are persisted.
+        /// Corresponding query that is completed when it reaches the end of the currently
+        /// stored events is provided by CurrentEventsByTagReader.
+        /// </summary>
+        EventsByTag,
+        /// <summary>
+        /// Same type of query as EventsByTagReader but the event stream
+        /// is completed immediately when it reaches the end of the "result set". Events that are
+        /// stored after the query is completed are not included in the event stream.
+        /// </summary>
+        CurrentEventsByTag
+    }
+
+    public class EventInput
+    {
+        public SourceFrom SourceFrom { get; }
+        public EventMethod EventMethod { get; }
+        public object[] InputArgs { get; }
+        public EventInput(SourceFrom sourceFrom, EventMethod eventMethod, object[] args)
+        {
+            SourceFrom = sourceFrom;
+            EventMethod = eventMethod;
+            InputArgs = args;
+        }
+    }
 }
