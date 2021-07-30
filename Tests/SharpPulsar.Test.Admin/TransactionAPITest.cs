550e0338360a579ea6952a9f769de2e9bc9456ff
Ebere Abanonu <eabanonu@yahoo.com>
2021-07-30 12:18:49 +0100

    First Admin release attempt

--- Tests/SharpPulsar.Test.Admin/TransactionAPITest.cs
+++ Tests/SharpPulsar.Test.Admin/TransactionAPITest.cs
@@ -1,5 +1,7 @@
 ﻿using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
+using SharpPulsar.Admin.Model;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -9,29 +11,32 @@ namespace SharpPulsar.Test.Admin
     public class TransactionAPITest
     {
         private readonly ITestOutputHelper _output;
-        private readonly User.Admin _admin;
+        private readonly SharpPulsar.Admin.Public.Admin _admin;
         public TransactionAPITest(ITestOutputHelper output)
         {
             _output = output;
-            _admin = new User.Admin("http://localhost:8080/", new HttpClient());
+            _admin = new SharpPulsar.Admin.Public.Admin("http://localhost:8080/", new HttpClient());
         }
         [Fact]
         public async Task Should_Get_Transaction_Metadata()
         {
-            var metadata = await _admin.GetTransactionMetadataWithHttpMessagesAsync(new SharpPulsar.Transaction.TxnID(3, 1));
-            Assert.True(true);
+            var metadata = await _admin.GetTransactionMetadataWithHttpMessagesAsync(new TxnID(3, 1));
+            _output.WriteLine(JsonSerializer.Serialize(metadata.Body));
+            Assert.True(metadata.Body != null);
         }
         [Fact]
         public async Task Should_Get_Transaction_Coordinators()
         {
             var coords = await _admin.GetCoordinatorStatsWithHttpMessagesAsync();
-            Assert.True(true);
+            _output.WriteLine(JsonSerializer.Serialize(coords.Body));
+            Assert.True(coords.Body != null);
         }
         [Fact]
         public async Task Should_Get_Transaction_Coordinator_by_Id()
         {
             var coord = await _admin.GetCoordinatorStatsByIdWithHttpMessagesAsync(0);
-            Assert.True(true);
+            _output.WriteLine(JsonSerializer.Serialize(coord.Body));
+            Assert.True(coord.Body != null);
         }
     }
 }
