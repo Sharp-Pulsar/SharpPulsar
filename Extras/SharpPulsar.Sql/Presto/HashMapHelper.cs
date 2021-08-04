ccaddcf03ed0e59498311bb86bf801a7ec786cc7
Ebere Abanonu <eabanonu@yahoo.com>
2021-08-04 14:50:20 +0100

    Make HashMapHelper public

--- Extras/SharpPulsar.Sql/Presto/HashMapHelper.cs
+++ Extras/SharpPulsar.Sql/Presto/HashMapHelper.cs
@@ -9,12 +9,12 @@ using System.Collections.Generic;
 
 namespace SharpPulsar.Sql.Presto
 {
-    internal static class HashMapHelper
+    public static class HashMapHelper
     {
         public static HashSet<KeyValuePair<TKey, TValue>> SetOfKeyValuePairs<TKey, TValue>(this IDictionary<TKey, TValue> dictionary)
         {
-            HashSet<KeyValuePair<TKey, TValue>> entries = new HashSet<KeyValuePair<TKey, TValue>>();
-            foreach (KeyValuePair<TKey, TValue> keyValuePair in dictionary)
+            var entries = new HashSet<KeyValuePair<TKey, TValue>>();
+            foreach (var keyValuePair in dictionary)
             {
                 entries.Add(keyValuePair);
             }
