270ea8c82c7931a7ce57ca9b4684497ffbe71705
Ebere Abanonu <eaba@users.noreply.github.com>
2022-08-04 14:59:38 +0100

    Nuke

---
+++ src/SharpPulsar.Test/API/Schema/KeyValueSchemaInfoTest.cs
@@ -0,0 +1,250 @@
+﻿using System.Collections.Generic;
+using SharpPulsar.Interfaces;
+using SharpPulsar.Interfaces.ISchema;
+using SharpPulsar.Schemas;
+using SharpPulsar.Shared;
+
+/// <summary>
+/// Licensed to the Apache Software Foundation (ASF) under one
+/// or more contributor license agreements.  See the NOTICE file
+/// distributed with this work for additional information
+/// regarding copyright ownership.  The ASF licenses this file
+/// to you under the Apache License, Version 2.0 (the
+/// "License"); you may not use this file except in compliance
+/// with the License.  You may obtain a copy of the License at
+/// 
+///   http://www.apache.org/licenses/LICENSE-2.0
+/// 
+/// Unless required by applicable law or agreed to in writing,
+/// software distributed under the License is distributed on an
+/// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+/// KIND, either express or implied.  See the License for the
+/// specific language governing permissions and limitations
+/// under the License.
+/// </summary>
+namespace SharpPulsar.Test.API.Schema
+{
+    
+    /// <summary>
+    /// Unit test <seealso cref="KeyValueSchemaInfoTest"/>.
+    /// </summary>
+    public class KeyValueSchemaInfoTest
+    {
+
+        private static readonly IDictionary<string, string> _fooProperties = new Dictionary<string, string>
+        {
+            {"foo1", "foo-value1" }, {"foo2", "foo-value2"}, {"foo3", "foo-value3"}
+        };
+
+        private static readonly IDictionary<string, string> _barProperties = new Dictionary<string, string>
+        {
+            {"bar1", "bar-value1" }, {"bar2", "bar-value2"}, {"bar3", "bar-value3"}
+        };
+
+        public static readonly ISchema<Foo> FooSchema = ISchema<object>.Avro(ISchemaDefinition<Foo>.Builder().WithAlwaysAllowNull(false).WithPojo(typeof(Foo)).WithProperties(_fooProperties).Build());
+        public static readonly ISchema<Bar> BarSchema = ISchema<object>.Json(ISchemaDefinition<Bar>.Builder().WithAlwaysAllowNull(true).WithPojo(typeof(Bar)).WithProperties(_barProperties).Build());
+
+        [Fact]
+        public void EncodeDecodeKeyValueSchemaInfoINLINE()
+        {
+            var encodingType = KeyValueEncodingType.INLINE;
+            var kvSchema = ISchema<object>.KeyValue(FooSchema, BarSchema, encodingType);
+            var kvSchemaInfo = kvSchema.SchemaInfo;
+            Assert.Equal(encodingType, DefaultImplementation.DecodeKeyValueEncodingType(kvSchemaInfo));
+
+            var encodedSchemaInfo = DefaultImplementation.EncodeKeyValueSchemaInfo(FooSchema, BarSchema, encodingType);
+            for (var i = 0; i < kvSchemaInfo.Schema.Length; i++)
+            {
+                var expected = kvSchemaInfo.Schema[i];
+                var actual = encodedSchemaInfo.Schema[i];
+                Assert.Equal(expected, actual);
+            }
+            Assert.Equal(encodingType, DefaultImplementation.DecodeKeyValueEncodingType(encodedSchemaInfo));
+
+            var schemaInfoKeyValue = DefaultImplementation.DecodeKeyValueSchemaInfo(kvSchemaInfo);
+
+            for (var i = 0; i < FooSchema.SchemaInfo.Schema.Length; i++)
+            {
+                var expected = FooSchema.SchemaInfo.Schema[i];
+                var actual = schemaInfoKeyValue.Key.Schema[i];
+                Assert.Equal(expected, actual);
+            }
+
+            for (var i = 0; i < BarSchema.SchemaInfo.Schema.Length; i++)
+            {
+                var expected = BarSchema.SchemaInfo.Schema[i];
+                var actual = schemaInfoKeyValue.Value.Schema[i];
+                Assert.Equal(expected, actual);
+            }
+        }
+
+        [Fact]
+        public void EncodeDecodeKeyValueSchemaInfoSEPARATED()
+        {
+            var encodingType = KeyValueEncodingType.SEPARATED;
+            var kvSchema = ISchema<object>.KeyValue(FooSchema, BarSchema, encodingType);
+            var kvSchemaInfo = kvSchema.SchemaInfo;
+            Assert.Equal(encodingType, DefaultImplementation.DecodeKeyValueEncodingType(kvSchemaInfo));
+
+            var encodedSchemaInfo = DefaultImplementation.EncodeKeyValueSchemaInfo(FooSchema, BarSchema, encodingType);
+            for (var i = 0; i < kvSchemaInfo.Schema.Length; i++)
+            {
+                var expected = kvSchemaInfo.Schema[i];
+                var actual = encodedSchemaInfo.Schema[i];
+                Assert.Equal(expected, actual);
+            }
+            Assert.Equal(encodingType, DefaultImplementation.DecodeKeyValueEncodingType(encodedSchemaInfo));
+
+            var schemaInfoKeyValue = DefaultImplementation.DecodeKeyValueSchemaInfo(kvSchemaInfo);
+
+            for (var i = 0; i < FooSchema.SchemaInfo.Schema.Length; i++)
+            {
+                var expected = FooSchema.SchemaInfo.Schema[i];
+                var actual = schemaInfoKeyValue.Key.Schema[i];
+                Assert.Equal(expected, actual);
+            }
+
+            for (var i = 0; i < BarSchema.SchemaInfo.Schema.Length; i++)
+            {
+                var expected = BarSchema.SchemaInfo.Schema[i];
+                var actual = schemaInfoKeyValue.Value.Schema[i];
+                Assert.Equal(expected, actual);
+            }
+        }
+
+
+        [Fact]
+        public void EncodeDecodeNestedKeyValueSchemaInfo()
+        {
+            var encodingType = KeyValueEncodingType.INLINE;
+            var nestedSchema = ISchema<object>.KeyValue(ISchema<object>.String, BarSchema, KeyValueEncodingType.INLINE);
+            var kvSchema = ISchema<object>.KeyValue(FooSchema, nestedSchema, encodingType);
+            var kvSchemaInfo = kvSchema.SchemaInfo;
+            Assert.Equal(encodingType, DefaultImplementation.DecodeKeyValueEncodingType(kvSchemaInfo));
+
+            var encodedSchemaInfo = DefaultImplementation.EncodeKeyValueSchemaInfo(FooSchema, nestedSchema, encodingType);
+            for (var i = 0; i < kvSchemaInfo.Schema.Length; i++)
+            {
+                var expected = kvSchemaInfo.Schema[i];
+                var actual = encodedSchemaInfo.Schema[i];
+                Assert.Equal(expected, actual);
+            }
+            Assert.Equal(encodingType, DefaultImplementation.DecodeKeyValueEncodingType(encodedSchemaInfo));
+
+            var schemaInfoKeyValue = DefaultImplementation.DecodeKeyValueSchemaInfo(kvSchemaInfo);
+
+
+            for (var i = 0; i < FooSchema.SchemaInfo.Schema.Length; i++)
+            {
+                var expected = FooSchema.SchemaInfo.Schema[i];
+                var actual = schemaInfoKeyValue.Key.Schema[i];
+                Assert.Equal(expected, actual);
+            }
+            Assert.Equal(schemaInfoKeyValue.Value.Type, SchemaType.KeyValue);
+            var nestedSchemaInfoKeyValue = DefaultImplementation.DecodeKeyValueSchemaInfo(schemaInfoKeyValue.Value);
+
+            var stringSchema = ISchema<object>.String.SchemaInfo.Schema;
+            for (var i = 0; i < stringSchema.Length; i++)
+            {
+                var expected = stringSchema[i];
+                var actual = nestedSchemaInfoKeyValue.Key.Schema[i];
+                Assert.Equal(expected, actual);
+            }
+            for (var i = 0; i < BarSchema.SchemaInfo.Schema.Length; i++)
+            {
+                var expected = BarSchema.SchemaInfo.Schema[i];
+                var actual = nestedSchemaInfoKeyValue.Value.Schema[i];
+                Assert.Equal(expected, actual);
+            }
+        }
+
+        [Fact]
+        public void EncodeDecodeNestedKeyValueSchemaInfoSEPARATED()
+        {
+            var encodingType = KeyValueEncodingType.SEPARATED;
+            var nestedSchema = ISchema<object>.KeyValue(ISchema<object>.String, BarSchema, KeyValueEncodingType.INLINE);
+            var kvSchema = ISchema<object>.KeyValue(FooSchema, nestedSchema, encodingType);
+            var kvSchemaInfo = kvSchema.SchemaInfo;
+            Assert.Equal(encodingType, DefaultImplementation.DecodeKeyValueEncodingType(kvSchemaInfo));
+
+            var encodedSchemaInfo = DefaultImplementation.EncodeKeyValueSchemaInfo(FooSchema, nestedSchema, encodingType);
+            for (var i = 0; i < kvSchemaInfo.Schema.Length; i++)
+            {
+                var expected = kvSchemaInfo.Schema[i];
+                var actual = encodedSchemaInfo.Schema[i];
+                Assert.Equal(expected, actual);
+            }
+            Assert.Equal(encodingType, DefaultImplementation.DecodeKeyValueEncodingType(encodedSchemaInfo));
+
+            var schemaInfoKeyValue = DefaultImplementation.DecodeKeyValueSchemaInfo(kvSchemaInfo);
+
+            for (var i = 0; i < FooSchema.SchemaInfo.Schema.Length; i++)
+            {
+                var expected = FooSchema.SchemaInfo.Schema[i];
+                var actual = schemaInfoKeyValue.Key.Schema[i];
+                Assert.Equal(expected, actual);
+            }
+            Assert.Equal(schemaInfoKeyValue.Value.Type, SchemaType.KeyValue);
+            var nestedSchemaInfoKeyValue = DefaultImplementation.DecodeKeyValueSchemaInfo(schemaInfoKeyValue.Value);
+
+            var stringSchema = ISchema<object>.String.SchemaInfo.Schema;
+            for (var i = 0; i < stringSchema.Length; i++)
+            {
+                var expected = stringSchema[i];
+                var actual = nestedSchemaInfoKeyValue.Key.Schema[i];
+                Assert.Equal(expected, actual);
+            }
+            for (var i = 0; i < BarSchema.SchemaInfo.Schema.Length; i++)
+            {
+                var expected = BarSchema.SchemaInfo.Schema[i];
+                var actual = nestedSchemaInfoKeyValue.Value.Schema[i];
+                Assert.Equal(expected, actual);
+            }
+        }
+
+        [Fact]
+        public virtual void TestKeyValueSchemaInfoBackwardCompatibility()
+        {
+            var kvSchema = ISchema<object>.KeyValue(FooSchema, BarSchema, KeyValueEncodingType.SEPARATED);
+
+            var oldSchemaInfo = new SchemaInfo
+            {
+
+                Name = "",
+                Type = SchemaType.KeyValue,
+                Schema = kvSchema.SchemaInfo.Schema,
+                Properties = new Dictionary<string, string>()
+            };
+
+            Assert.Equal(KeyValueEncodingType.INLINE, DefaultImplementation.DecodeKeyValueEncodingType(oldSchemaInfo));
+
+            var schemaInfoKeyValue = DefaultImplementation.DecodeKeyValueSchemaInfo(oldSchemaInfo);
+            // verify the key schemaS
+            var keySchemaInfo = schemaInfoKeyValue.Key;
+            Assert.Equal(SchemaType.BYTES, keySchemaInfo.Type);
+
+            for (var i = 0; i < FooSchema.SchemaInfo.Schema.Length; i++)
+            {
+                var expected = FooSchema.SchemaInfo.Schema[i];
+                var actual = keySchemaInfo.Schema[i];
+                Assert.Equal(expected, actual);
+            }
+
+            Assert.False(FooSchema.SchemaInfo.Properties.Count == 0);
+            Assert.True(keySchemaInfo.Properties.Count == 0);
+            // verify the value schema
+            var valueSchemaInfo = schemaInfoKeyValue.Value;
+            Assert.Equal(SchemaType.BYTES, valueSchemaInfo.Type);
+            for (var i = 0; i < BarSchema.SchemaInfo.Schema.Length; i++)
+            {
+                var expected = BarSchema.SchemaInfo.Schema[i];
+                var actual = valueSchemaInfo.Schema[i];
+                Assert.Equal(expected, actual);
+            }
+            Assert.False(BarSchema.SchemaInfo.Properties.Count == 0);
+            Assert.True(valueSchemaInfo.Properties.Count == 0);
+        }
+
+    }
+
+}
---
+++ src/SharpPulsar.Test/MultiTopic/MultiTopicsReaderTest.cs
@@ -0,0 +1,164 @@
+﻿using SharpPulsar.User;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit.Abstractions;
+using SharpPulsar.Interfaces;
+using SharpPulsar.TestContainer;
+using SharpPulsar.Builder;
+using SharpPulsar.Test.Fixture;
+using System.Threading.Tasks;
+/// <summary>
+/// Licensed to the Apache Software Foundation (ASF) under one
+/// or more contributor license agreements.  See the NOTICE file
+/// distributed with this work for additional information
+/// regarding copyright ownership.  The ASF licenses this file
+/// to you under the Apache License, Version 2.0 (the
+/// "License"); you may not use this file except in compliance
+/// with the License.  You may obtain a copy of the License at
+/// 
+///   http://www.apache.org/licenses/LICENSE-2.0
+/// 
+/// Unless required by applicable law or agreed to in writing,
+/// software distributed under the License is distributed on an
+/// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+/// KIND, either express or implied.  See the License for the
+/// specific language governing permissions and limitations
+/// under the License.
+/// </summary>
+namespace SharpPulsar.Test.MultiTopics
+{
+
+    [Collection(nameof(PulsarCollection))]
+	public class MultiTopicsReaderTest 
+    {
+
+		private const string Subscription = "reader-multi-topics-sub";
+		private readonly ITestOutputHelper _output;
+        private readonly PulsarClient _client;
+        public MultiTopicsReaderTest(ITestOutputHelper output, PulsarFixture fixture)
+		{
+			_output = output;
+            _client = fixture.Client;
+        }
+		[Fact]
+		public virtual async Task TestReadMessageWithoutBatching()
+		{
+			var topic = "ReadMessageWithoutBatching";
+			await TestReadMessages(topic, false);
+		}
+		[Fact]
+		public virtual async Task TestReadMessageWithBatching()
+		{
+			var topic = $"ReadMessageWithBatching_{Guid.NewGuid()}";
+			await TestReadMessages(topic, true);
+		}
+		[Fact]
+		public async Task TestMultiTopic()
+		{
+			var topic = "persistent://public/default/topic" + Guid.NewGuid();
+
+			var topic2 = "persistent://public/default/topic2" + Guid.NewGuid();
+
+			var topic3 = "persistent://public/default/topic3" + Guid.NewGuid();
+			IList<string> topics = new List<string> { topic, topic2, topic3 };
+			var builder = new ReaderConfigBuilder<string>()
+				.Topics(topics)
+				.StartMessageId(IMessageId.Earliest)
+                
+				.ReaderName("my-reader");
+
+			var reader = await _client.NewReaderAsync(ISchema<object>.String, builder);
+			// create producer and send msg
+			IList<Producer<string>> producerList = new List<Producer<string>>();
+			foreach (var topicName in topics)
+			{
+                var producer = await _client.NewProducerAsync(ISchema<object>.String, new ProducerConfigBuilder<string>().Topic(topicName));
+
+				producerList.Add(producer);
+			}
+			var msgNum = 10;
+			ISet<string> messages = new HashSet<string>();
+			for (var i = 0; i < producerList.Count; i++)
+			{
+				var producer = producerList[i];
+				for (var j = 0; j < msgNum; j++)
+				{
+					var msg = i + "msg" + j;
+					await producer.SendAsync(msg);
+					messages.Add(msg);
+				}
+			}
+			// receive messagesS
+			var message = await reader.ReadNextAsync(TimeSpan.FromSeconds(5));
+			while (message != null)
+			{
+				var value = message.Value;
+				_output.WriteLine(value);
+				Assert.True(messages.Remove(value));
+				message = await reader.ReadNextAsync(TimeSpan.FromSeconds(5));
+			}
+			Assert.True(messages.Count == 0 || messages.Count == 1);
+			// clean up
+			foreach (var producer in producerList)
+			{
+				await producer.CloseAsync();
+			}
+			await reader.CloseAsync();
+		}
+		private async Task TestReadMessages(string topic, bool enableBatch)
+		{
+			var numKeys = 10;
+            var builder = new ReaderConfigBuilder<byte[]>()
+                .Topic(topic)
+                
+                .StartMessageId(IMessageId.Earliest)
+                .ReaderName(Subscription);
+            var reader = await _client.NewReaderAsync(builder);
+
+            var keys = await PublishMessages(topic, numKeys, enableBatch);
+			await Task.Delay(TimeSpan.FromSeconds(5));
+			for (var i = 0; i < numKeys; i++)
+			{
+				var message = await reader.ReadNextAsync();
+				if (message != null)
+				{
+					_output.WriteLine($"{message.Key}:{message.MessageId}:{Encoding.UTF8.GetString(message.Data)}");
+					Assert.True(keys.Remove(message.Key));
+				}
+			}
+			Assert.True(keys.Count == 0);
+		}
+		private async Task<ISet<string>> PublishMessages(string topic, int count, bool enableBatch)
+		{
+			ISet<string> keys = new HashSet<string>();
+			var builder = new ProducerConfigBuilder<byte[]>()
+				.Topic(topic)
+				.MessageRoutingMode(Common.MessageRoutingMode.RoundRobinMode)
+				.MaxPendingMessages(count)
+				.BatchingMaxPublishDelay(TimeSpan.FromMilliseconds(80000));
+			if (enableBatch)
+			{
+				builder.EnableBatching(true);
+				builder.BatchingMaxMessages(count);
+			}
+			else
+			{
+				builder.EnableBatching(false);
+			}
+
+			var producer = await _client.NewProducerAsync(builder);
+			for (var i = 0; i < count; i++)
+			{
+				var key = "key" + i;
+				var data = Encoding.UTF8.GetBytes("my-message-" + i);
+				await producer.NewMessage().Key(key).Value(data).SendAsync();
+				keys.Add(key);
+			}
+			producer.Flush();
+			return keys;
+		}
+        
+    }
+
+}
