cef91444b203c65a2bc3e91679175d71a4c28947
Ebere Abanonu <eaba@users.noreply.github.com>
2022-10-07 01:17:03 +0100

    Admin Test [ci skip]

---
+++ src/SharpPulsar.Admin.Test/Transaction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using SharpPulsar.Admin.v3;
+using SharpPulsar.Admin.v3.Transactions;
+using Xunit.Abstractions;
+
+namespace SharpPulsar.Admin.Test
+{
+    [Collection("pulsar admin")]
+    public class Transaction
+    {
+        private readonly ITestOutputHelper _output;
+        private PulsarTransactionsRESTAPIClient _admin;
+        private JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true
+        };
+        public Transaction(ITestOutputHelper output)
+        {
+            _output = output;
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri("http://localhost:8080/admin/v3/")
+            };
+            _admin = new PulsarTransactionsRESTAPIClient(client);
+        }
+    }
+}
