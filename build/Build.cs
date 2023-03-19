e2ab418db79044a897acd1e6bead7e859cc37c71
Ebere Abanonu <eaba@users.noreply.github.com>
2023-03-19 03:47:04 +0100

    update

--- build/Build.cs
+++ build/Build.cs
@@ -125,7 +125,8 @@ partial class Build : NukeBuild
                 Git($"tag -f {GitVersion.MajorMinorPatch}");
                 //git push --atomic origin <branch name> <tag>
                 //https://gist.github.com/danielestevez/2044589
-                //https://www.atlassian.com/git/tutorials/merging-vs-rebasing
+                //https://www.atlassian.com/git/tutorials/merging-vs-rebasing./build.cmd createnuget
+                //https://git-scm.com/docs/git-push
             }
         });
 
