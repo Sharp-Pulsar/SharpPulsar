2ade272950e370f34d9ba35c9bcd7c3a584748bf
Ebere Abanonu <eabanonu@yahoo.com>
2023-04-30 23:41:14 +0100

    git

--- build/Build.cs
+++ build/Build.cs
@@ -141,6 +141,7 @@ partial class Build : NukeBuild
                 Git($"commit -S -m \"Finalize {Path.GetFileName(ChangelogFile)} for {GitVersion.MajorMinorPatch}.\"");
 
                 Git($"tag -f {GitVersion.MajorMinorPatch}");
+
                 //git push --atomic origin <branch name> <tag>
                 //https://gist.github.com/danielestevez/2044589
                 //https://www.atlassian.com/git/tutorials/merging-vs-rebasing./build.cmd createnuget
