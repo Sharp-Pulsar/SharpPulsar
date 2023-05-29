a3ba6ba93f2d1fa31a5b28bd1fb063d46e557eda
Ebere Abanonu <eabanonu@yahoo.com>
2023-05-29 12:48:43 +0100

    update

--- build/Build.CI.GitHubActions.cs
+++ build/Build.CI.GitHubActions.cs
@@ -59,6 +59,7 @@ public class CustomGitHubActionsAttribute : GitHubActionsAttribute
     protected override GitHubActionsJob GetJobs(GitHubActionsImage image, IReadOnlyCollection<ExecutableTarget> relevantTargets)
     {
         var job = base.GetJobs(image, relevantTargets);
+        
         var newSteps = new List<GitHubActionsStep>(job.Steps);
         //newSteps.Insert(1, new GitHubActionsUploadArtifact{ });
         foreach (var version in new[] { "7.0.*"})
