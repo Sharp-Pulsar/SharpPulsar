9666c14e55015a87b2f9d19eaf3f983432cda1a5
Ebere Abanonu <eabanonu@yahoo.com>
2023-09-22 10:58:54 +0100

    [REMOVED][Trino] `HttpRequest<T>` `WebRequest.Create(Uri)`

--- src/SharpPulsar.Trino/Trino/Helper.cs
+++ src/SharpPulsar.Trino/Trino/Helper.cs
@@ -1,56 +1,10 @@
 ﻿using System.Net;
-using System.Text;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace SharpPulsar.Trino.Trino
 {
     internal class Helper
-    {
-        internal static T HttpRequest<T>(string Method, string Url, string Request = "", WebHeaderCollection Headers = null)
-        {
-            var http = WebRequest.Create(new Uri(Url));
-            http.Method = Method;
-
-            if (Headers != null)
-                http.Headers.Add(Headers);
-            http.Credentials = CredentialCache.DefaultCredentials;
-
-            if (!string.IsNullOrEmpty(Request))
-            {
-                var encoding = new ASCIIEncoding();
-                var bytes = encoding.GetBytes(Request);
-
-                using var newStream = http.GetRequestStream();
-                newStream.Write(bytes, 0, bytes.Length);
-                newStream.Close();
-            }
-            try
-            {
-                using var response = http.GetResponse();
-                using var stream = response.GetResponseStream();
-                if (stream != null)
-                {
-                    using var sr = new StreamReader(stream);
-                    var str = sr.ReadToEnd();
-                    return JsonConvert.DeserializeObject<T>(str);
-                }
-                throw new NullReferenceException();
-            }
-            catch (WebException ex)
-            {
-                if (ex.Response.ContentLength > 0)
-                {
-                    using var stream = ex.Response.GetResponseStream();
-                    var sr = new StreamReader(stream);
-                    throw new Exception("ERROR: " + sr.ReadToEnd());
-                }
-
-                throw new Exception("ERROR: " + ex.Message);
-
-            }
-        }
-
+    {       
         internal static DateTime ParseTimeWithTimeZone(string Data)
         {
             var iSep = Data.IndexOf(' ');
